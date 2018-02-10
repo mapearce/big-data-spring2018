8ec767380a4664fc40d2bf1f400175699245f000
Michael <mapearce@mit.edu>
2018-02-10 14:31:11 -0500

    Uppered the first letter of each line

--- week-01/submission/first-script.py
+++ week-01/submission/first-script.py
@@ -1,6 +1,6 @@
 funtime = input("What is your name? ")
 msg = "This is a fun new message care of Michael Pearce"
 for i in funtime:
-    print(i + " is for the " + i + " in " + funtime)
+    print(i.upper() + " is for the " + i + " in " + funtime)
 print("GOOOOO " + funtime.upper() + "!!!!")
 print(msg)
